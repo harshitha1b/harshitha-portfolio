ae3bc4814eee44e2d42319d75fcc8d1f21a4ea8a
Harsh <harshithaboggula9@gmail.com>
2026-07-17 00:05:00 +0530

    remove developerFolio footer

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -14,13 +14,7 @@ export default function Footer() {
         </p>
         <p className={isDark ? "dark-mode footer-text" : "footer-text"}>
           Theme by{" "}
-          <a
-            href="https://github.com/saadpasta/developerFolio"
-            target="_blank"
-            rel="noreferrer"
-          >
-            developerFolio
-          </a>
+          
         </p>
       </div>
     </Fade>
